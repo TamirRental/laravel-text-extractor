13c4b8bad96c1306e8e2dd50a615849df06af431
ankitkabra-bacancy <ankit.kabra@bacancy.com>
2026-02-17 22:20:11 +0530

    Remove declare(strict_types=1) from all files

--- src/Listeners/ProcessDocumentExtraction.php
+++ src/Listeners/ProcessDocumentExtraction.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace TamirRental\DocumentExtraction\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
--- src/PendingExtraction.php
+++ src/PendingExtraction.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace TamirRental\DocumentExtraction;
 
 use Illuminate\Support\Traits\Conditionable;
